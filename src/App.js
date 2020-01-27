6791b231247ad1c9e331d5ddf40c3791e09ec744
rashmi <rashmi.manandhar7@gmail.com>
2020-01-26 21:33:32 -0500

    connect with API using lifecycle hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import React, {Component} from 'react';
 import CardContacts from './CardContacts';
+import * as ContactsAPI from './utils/ContactsAPI'
 
+console.log(ContactsAPI);
 class App extends Component {
 	state = {
-		contacts: [
-			{
-				"id": "karen",
-				"name": "Karen Isgrigg",
-				"handle": "karen_isgrigg",
-				"avatarURL": "http://localhost:5001/karen.jpg"
-			}, {
-				"id": "richard",
-				"name": "Richard Kalehoff",
-				"handle": "richardkalehoff",
-				"avatarURL": "http://localhost:5001/richard.jpg"
-			}, {
-				"id": "tyler",
-				"name": "Tyler McGinnis",
-				"handle": "tylermcginnis",
-				"avatarURL": "http://localhost:5001/tyler.jpg"
-			}
-		]
-
+		contacts: []
 	}
 
+	componentDidMount() {
+		ContactsAPI.getAll().then((contacts) => {
+			this.setState(() => ({contacts}))
+		})
+	}
 	removeContact = (contact) => {
 		this.setState((currentState) => ({
 			contacts: currentState.contacts.filter((c) => {
 				return c.id !== contact.id
 			})
 		}))
+
+		ContactsAPI.remove(contact);
 	}
 
 	render() {
